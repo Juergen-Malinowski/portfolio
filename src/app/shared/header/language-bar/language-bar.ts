cad9542fd71229c8caf4e69d76c5c72af0ec769f
Jürgen Malinowski <jm-malle@gmx.de>
2025-11-14 20:02:14 +0100

    Generated additional components for the header (nav-bar, language-bar). Created initial SCSS files.

---
+++ src/app/shared/header/language-bar/language-bar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-language-bar',
+  imports: [],
+  templateUrl: './language-bar.html',
+  styleUrl: './language-bar.scss',
+})
+export class LanguageBar {
+
+}
