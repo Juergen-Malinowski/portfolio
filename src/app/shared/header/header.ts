60e62eb9cbe877c6cb05d2c33a7032ea69e34b9e
Jürgen Malinowski <jm-malle@gmx.de>
2025-11-14 22:04:21 +0100

    working on header-design

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { NavBar } from './nav-bar/nav-bar';
+import { LanguageBar } from './language-bar/language-bar';
+
+/* Unter-Komponenten importieren ... */
+
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [NavBar, LanguageBar ],
   templateUrl: './header.html',
-  styleUrl: './header.scss',
+  styleUrl: './header.scss'
 })
 export class Header {
 
+  constructor() {
+      // Konstruktor ohne Logik ...
+  }
+
 }
+
