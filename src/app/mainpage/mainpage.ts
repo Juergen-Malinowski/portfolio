7496d1344a9832f9d4d70cd4e8d361f932631bf1
Jürgen Malinowski <jm-malle@gmx.de>
2025-11-18 13:42:25 +0100

    working on neu structure ... working on maingpage

--- src/app/mainpage/mainpage.ts
+++ src/app/mainpage/mainpage.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { Landingpage } from './landingpage/landingpage';
+import { About } from './about/about';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [],
+  imports: [Landingpage, About],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.scss',
 })
