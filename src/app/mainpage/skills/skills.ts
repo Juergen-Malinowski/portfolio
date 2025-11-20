a515b47f0aa7c7f15c5a952f7d0d41d7225be172
Jürgen Malinowski <jm-malle@gmx.de>
2025-11-20 13:52:48 +0100

    My Skills section pre-designed.

--- src/app/mainpage/skills/skills.ts
+++ src/app/mainpage/skills/skills.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  standalone: true,
   templateUrl: './skills.html',
-  styleUrl: './skills.scss',
+  styleUrl: './skills.scss'
 })
-export class Skills {
+export class Skills implements AfterViewInit {
 
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit(): void {
+    this.initSkillIconAnimation();
+  }
+
+  private initSkillIconAnimation(): void {
+    var icons = this.elementRef.nativeElement.querySelectorAll('.skill-icon');
+
+    for (var i = 0; i < icons.length; i++) {
+      var icon = icons[i] as HTMLElement;
+
+      icon.addEventListener('mousemove', function (this: HTMLElement) {
+
+        /* Animationsklasse entfernen */
+        this.classList.remove('skill-icon-anim');
+
+        /* Reflow erzwingen → Browser „vergisst“ alte Animation */
+        void this.offsetWidth;
+
+        /* Animationsklasse erneut hinzufügen → Animation läuft NEU */
+        this.classList.add('skill-icon-anim');
+      });
+    }
+  }
 }
