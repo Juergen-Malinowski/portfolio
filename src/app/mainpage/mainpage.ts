e831392f4391077c8063acc4dcdcd537ced6a08c
Jürgen Malinowski <jm-malle@gmx.de>
2025-11-20 09:59:56 +0100

    Next section: ‘Skills’. Graphics provided. Start design...

--- src/app/mainpage/mainpage.ts
+++ src/app/mainpage/mainpage.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Landingpage } from './landingpage/landingpage';
 import { About } from './about/about';
+import { Skills } from './skills/skills';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [Landingpage, About],
+  imports: [Landingpage, About, Skills ],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.scss',
 })
