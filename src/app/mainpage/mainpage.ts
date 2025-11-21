3356093b4784511cf01ac72ee2a4cdf1ec97f486
Jürgen Malinowski <jm-malle@gmx.de>
2025-11-21 10:53:23 +0100

    Next section ‘projects’ linked

--- src/app/mainpage/mainpage.ts
+++ src/app/mainpage/mainpage.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Landingpage } from './landingpage/landingpage';
 import { About } from './about/about';
 import { Skills } from './skills/skills';
+import { Projects } from './projects/projects';
 
 @Component({
   selector: 'app-mainpage',
-  imports: [Landingpage, About, Skills ],
+  imports: [Landingpage, About, Skills, Projects ],
   templateUrl: './mainpage.html',
   styleUrl: './mainpage.scss',
 })
