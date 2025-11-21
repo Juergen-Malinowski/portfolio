ac99ed8093983ad72daaa11f97ced7b8d715f0f6
Jürgen Malinowski <jm-malle@gmx.de>
2025-11-21 10:41:35 +0100

    Header ID jump added for skills and graphics improved

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -2,21 +2,27 @@ import { Component } from '@angular/core';
 import { NavBar } from './nav-bar/nav-bar';
 import { LanguageBar } from './language-bar/language-bar';
 
-/* Unter-Komponenten importieren ... */
-
-
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavBar, LanguageBar ],
+  imports: [NavBar, LanguageBar],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
 
   constructor() {
-      // Konstruktor ohne Logik ...
+    // Konstruktor ohne Logik ...
+  }
+
+  /* ============================= */
+  /*   Zur Landingpage scrollen ... */
+  /* ============================= */
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
   }
 
 }
-
--- src/app/shared/header/nav-bar/nav-bar.ts
+++ src/app/shared/header/nav-bar/nav-bar.ts
@@ -4,9 +4,24 @@ import { TranslatePipe } from '@ngx-translate/core';
 @Component({
   selector: 'app-nav-bar',
   imports: [TranslatePipe],
+  standalone: true,
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.scss',
 })
 export class NavBar {
+
+  scrollToAbout() {
+    const aboutSection = document.getElementById('about');
+    if (aboutSection) {
+      aboutSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
+
+  scrollToSkills() {
+    const section = document.getElementById('skills');
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 
 }
